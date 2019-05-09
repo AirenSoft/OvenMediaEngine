5aff1e90a14d9cd911e037926c0b594a5dbfe9d6
dimiden <dimiden@airensoft.com>
2019-05-09 12:46:40 +0900

    Removed build phrase for precompiled header

--- src/projects/base/ovlibrary/AMS.mk
+++ src/projects/base/ovlibrary/AMS.mk
@@ -3,32 +3,7 @@ include $(DEFAULT_VARIABLES)
 
 LOCAL_TARGET := ovlibrary
 
-LOCAL_HEADER_FILES := $(LOCAL_HEADER_FILES) \
-	projects/base/ovlibrary/ovlibrary.h.gch
-
-LOCAL_SOURCE_FILES := $(LOCAL_SOURCE_FILES) \
-	projects/base/ovlibrary/ovlibrary.h.gch
-
-# =============
-# Precompiled Header (temporary solution)
-LOCAL_PRECOMPILE_FILE := $(LOCAL_PATH)/ovlibrary.h
-LOCAL_PRECOMPILE_TEMP_DEP := $(LOCAL_PATH)/log.cpp
-
-# Dummy dependency
-LOCAL_PRECOMPILE_TARGET := $(LOCAL_PRECOMPILE_FILE).gch
-LOCAL_PRECOMPILE_DEPS := $(LOCAL_PRECOMPILE_FILE).P
-
-$(LOCAL_PRECOMPILE_TEMP_DEP) : $(LOCAL_PRECOMPILE_TARGET)
-
-$(LOCAL_PRECOMPILE_DEPS):
-	@echo "Generating precompiled header dependencies for $@..."
-	@g++ -I../ -I../third_party -MM $(LOCAL_PRECOMPILE_FILE) -MT $(LOCAL_PRECOMPILE_TARGET) -o $@
-
-$(LOCAL_PRECOMPILE_TARGET) : $(LOCAL_PRECOMPILE_DEPS)
-	@echo "Creating precompiled header $@..."
-	@g++ -o $@ $(LOCAL_PRECOMPILE_FILE) -Iprojects/ -Iprojects/third_party
-
-include $(LOCAL_PRECOMPILE_DEPS)
-# =============
+LOCAL_HEADER_FILES := $(LOCAL_HEADER_FILES)
+LOCAL_SOURCE_FILES := $(LOCAL_SOURCE_FILES)
 
 include $(BUILD_STATIC_LIBRARY)
--- src/projects/config/AMS.mk
+++ src/projects/config/AMS.mk
@@ -13,51 +13,6 @@ LOCAL_SOURCE_FILES := $(LOCAL_SOURCE_FILES) \
 
 LOCAL_HEADER_FILES := $(LOCAL_HEADER_FILES) \
     $(call get_sub_header_list,items) \
-    $(call get_sub_header_list,utilities) \
-    config.h.gch
-
-# =============
-# Precompiled Header (temporary solution)
-LOCAL_PRECOMPILE_FILE := $(LOCAL_PATH)/item.h
-LOCAL_PRECOMPILE_TEMP_DEP := $(LOCAL_PATH)/config_loader.cpp
-
-# Dummy dependency
-LOCAL_PRECOMPILE_TARGET := $(LOCAL_PRECOMPILE_FILE).gch
-LOCAL_PRECOMPILE_DEPS := $(LOCAL_PRECOMPILE_FILE).P
-
-$(LOCAL_PRECOMPILE_TEMP_DEP) : $(LOCAL_PRECOMPILE_TARGET)
-
-$(LOCAL_PRECOMPILE_DEPS):
-	@echo "Generating precompiled header dependencies for $@..."
-	@g++ -I../ -I../third_party -MM $(LOCAL_PRECOMPILE_FILE) -MT $(LOCAL_PRECOMPILE_TARGET) -o $@
-
-$(LOCAL_PRECOMPILE_TARGET) : $(LOCAL_PRECOMPILE_DEPS)
-	@echo "Creating precompiled header $@..."
-	@g++ -o $@ $(LOCAL_PRECOMPILE_FILE) -Iprojects/ -Iprojects/third_party
-
-include $(LOCAL_PRECOMPILE_DEPS)
-# =============
-
-# =============
-# Precompiled Header (temporary solution)
-LOCAL_PRECOMPILE_FILE := $(LOCAL_PATH)/config.h
-LOCAL_PRECOMPILE_TEMP_DEP := $(LOCAL_PATH)/config_loader.cpp
-
-# Dummy dependency
-LOCAL_PRECOMPILE_TARGET := $(LOCAL_PRECOMPILE_FILE).gch
-LOCAL_PRECOMPILE_DEPS := $(LOCAL_PRECOMPILE_FILE).P
-
-$(LOCAL_PRECOMPILE_TEMP_DEP) : $(LOCAL_PRECOMPILE_TARGET)
-
-$(LOCAL_PRECOMPILE_DEPS):
-	@echo "Generating precompiled header dependencies for $@..."
-	@g++ -I../ -I../third_party -MM $(LOCAL_PRECOMPILE_FILE) -MT $(LOCAL_PRECOMPILE_TARGET) -o $@
-
-$(LOCAL_PRECOMPILE_TARGET) : $(LOCAL_PRECOMPILE_DEPS)
-	@echo "Creating precompiled header $@..."
-	@g++ -o $@ $(LOCAL_PRECOMPILE_FILE) -Iprojects/ -Iprojects/third_party
-
-include $(LOCAL_PRECOMPILE_DEPS)
-# =============
+    $(call get_sub_header_list,utilities)
 
 include $(BUILD_STATIC_LIBRARY)
