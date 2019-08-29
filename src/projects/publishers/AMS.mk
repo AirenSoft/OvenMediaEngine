9c981207e08ccb2193edb33b6dd8ac1c6f8e7c73
Dimiden <dimiden@airensoft.com>
2019-08-30 02:32:02 +0900

    Add a configuration for SRT

--- src/projects/publishers/AMS.mk
+++ src/projects/publishers/AMS.mk
@@ -4,13 +4,15 @@ include $(DEFAULT_VARIABLES)
 LOCAL_TARGET := publishers
 
 LOCAL_SOURCE_FILES := $(LOCAL_SOURCE_FILES) \
-    $(call get_sub_source_list,hls) \
+	$(call get_sub_source_list,hls) \
     $(call get_sub_source_list,dash) \
 	$(call get_sub_source_list,cmaf)
 
 LOCAL_HEADER_FILES := $(LOCAL_HEADER_FILES) \
-    $(call get_sub_header_list,hls) \
+	$(call get_sub_header_list,hls) \
 	$(call get_sub_header_list,dash) \
 	$(call get_sub_header_list,cmaf)
+
+$(call add_pkg_config,srt)
 
 include $(BUILD_STATIC_LIBRARY)
