919828fed78e3da2574368df45ac1ad095ab6729
Kwon Keuk Han <soulk@airensoft.com>
2020-01-13 23:37:12 +0900

    delete unused functions

--- src/projects/media_router/bitstream/avc_video_packet_fragmentizer.h
+++ src/projects/media_router/bitstream/avc_video_packet_fragmentizer.h
@@ -10,11 +10,6 @@ class AvcVideoPacketFragmentizer
 {
 public:
 	bool MakeHeader(std::shared_ptr<MediaPacket> packet);
-	
-#if 1
-	FragmentationHeader* FromAvcVideoPacket2(std::shared_ptr<MediaPacket> packet);
-	FragmentationHeader* FromAvcVideoPacket(const uint8_t *packet, size_t length);
-#endif
 
 private:
     uint8_t nal_length_size_ = 0;
