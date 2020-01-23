211d12c71163c6b84c37f247184efa57f7ea8182
Dimiden <dimiden@airensoft.com>
2020-01-23 20:12:30 +0900

    Added pkg_config dependency for RTSP Provider

--- src/projects/providers/rtsp/AMS.mk
+++ src/projects/providers/rtsp/AMS.mk
@@ -22,7 +22,7 @@ LOCAL_HEADER_FILES := $(LOCAL_HEADER_FILES) \
     $(call get_sub_header_list,rtp) \
     $(call get_sub_header_list,rtcp)
 
-LOCAL_CFLAGS := $(shell pkg-config --cflags srt)
-LOCAL_CXXFLAGS := $(shell pkg-config --cflags srt)
+$(call add_pkg_config,openssl)
+$(call add_pkg_config,srt)
 
 include $(BUILD_STATIC_LIBRARY)
