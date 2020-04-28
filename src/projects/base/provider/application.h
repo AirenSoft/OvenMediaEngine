572f0b6766f3695ca1a12d0649bbb7b646c2dc23
getroot <getroot@airensoft.com>
2020-04-28 17:33:50 +0900

    Remove incorrectly merged code

--- src/projects/base/provider/application.h
+++ src/projects/base/provider/application.h
@@ -62,7 +62,6 @@ namespace pvd
 		virtual bool Start();
 		virtual bool Stop();
 
-		const std::map<uint32_t, std::shared_ptr<Stream>> GetStreams();
 		const std::shared_ptr<Stream> GetStreamById(uint32_t stream_id);
 		const std::shared_ptr<Stream> GetStreamByName(ov::String stream_name);
 
