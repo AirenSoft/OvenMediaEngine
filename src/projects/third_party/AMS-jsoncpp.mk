21f8804352907853433aca1bfb3017fb82f4aab5
Dimiden <dimiden@airensoft.com>
2020-06-01 14:51:10 +0900

    Changed jsoncpp to v1.9.3

--- src/projects/third_party/AMS-jsoncpp.mk
+++ src/projects/third_party/AMS-jsoncpp.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call get_local_path)
 
 include $(CLEAR_VARIABLES)
 
-SOURCE_PATH := jsoncpp-1.8.4
+SOURCE_PATH := jsoncpp-1.9.3
 
 LOCAL_TARGET := jsoncpp
 LOCAL_SOURCE_FILES := $(call get_sub_source_list,$(SOURCE_PATH))
