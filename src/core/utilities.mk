9367c1eda4f8ae53dcddc5327f9400d07ad249d3
Dimiden <dimiden@airensoft.com>
2020-10-27 17:21:18 +0900

    Added some util functions

--- src/core/utilities.mk
+++ src/core/utilities.mk
@@ -1,4 +1,6 @@
-# 현재 Makefile의 경로를 얻어옴
+#
+# Obtain the current path of Makefile
+#
 define get_local_path
 $(strip \
 	$(eval current_makefile := $$(lastword $$(MAKEFILE_LIST))) \
@@ -9,8 +11,11 @@ $(strip \
 )
 endef
 
-# LOCAL_PATH 안의 파일 목록을 얻어옴
-# $(call get_local_file_list,<패턴>)
+#
+# Get a list of files in LOCAL_PATH
+#
+# Usage: $(call get_local_file_list,<pattern>)
+# Ex.:   $(call get_local_file_list,*.cpp)
 define get_local_file_list
 $(strip \
 	$(patsubst $(LOCAL_PATH)/%,%,
@@ -22,7 +27,10 @@ $(strip \
 )
 endef
 
-# LOCAL_PATH 안의 소스 파일 목록을 얻어옴
+#
+# Get a list of c/c++ files in LOCAL_PATH
+#
+# Usage: $(call get_local_source_list)
 define get_local_source_list
 $(strip \
 	$(call get_local_file_list,*.$(patsubst .%,%,$(CONFIG_C_EXTENSION))) \
@@ -30,16 +38,30 @@ $(strip \
 )
 endef
 
-# LOCAL_PATH 안의 헤더 파일 목록을 얻어옴
-define get_local_header_list
-$(strip \
-	$(call get_local_file_list,*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
-	$(call get_local_file_list,*.$(patsubst .%,%,$(CONFIG_CXX_HEADER_EXTENSION))) \
-)
-endef
+#
+# Get a list of header files in LOCAL_PATH
+#
+# Usage: $(call get_local_header_list)
+ifeq ($(CONFIG_C_HEADER_EXTENSION),$(CONFIG_CXX_HEADER_EXTENSION))
+    define get_local_header_list
+    $(strip \
+    	$(call get_local_file_list,*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
+    )
+    endef
+else
+    define get_local_header_list
+    $(strip \
+    	$(call get_local_file_list,*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
+    	$(call get_local_file_list,*.$(patsubst .%,%,$(CONFIG_CXX_HEADER_EXTENSION))) \
+    )
+    endef
+endif
 
-# 하위 디렉토리의 파일 목록을 얻어옴
-# $(call get_sub_file_list,<경로>,<패턴>)
+#
+# Get a list of files in LOCAL_PATH/<path>
+#
+# Usage: $(call get_sub_file_list,<path>,<pattern>)
+# Ex.:   $(call get_sub_file_list,sub_dir,*.cpp)
 define get_sub_file_list
 $(strip \
 	$(patsubst $(LOCAL_PATH)/%,%,
@@ -51,8 +73,11 @@ $(strip \
 )
 endef
 
-# 하위 디렉토리의 소스 코드 목록을 얻어옴
-# $(call get_sub_source_list,<경로>)
+#
+# Get a list of c/c++ files in LOCAL_PATH/<path>
+#
+# Usage: $(call get_sub_source_list,<path>)
+# Ex.:   $(call get_sub_source_list,sub_dir)
 define get_sub_source_list
 $(strip \
 	$(call get_sub_file_list,$(1),*.$(patsubst .%,%,$(CONFIG_C_EXTENSION))) \
@@ -60,17 +85,97 @@ $(strip \
 )
 endef
 
-# 하위 디렉토리의 소스 코드 목록을 얻어옴
-# $(call get_sub_header_list,<경로>)
-define get_sub_header_list
+#
+# Get a list of header files in LOCAL_PATH/<path>
+#
+# Usage: $(call get_sub_header_list,<path>)
+# Ex.:   $(call get_sub_header_list,sub_dir)
+ifeq ($(CONFIG_C_HEADER_EXTENSION),$(CONFIG_CXX_HEADER_EXTENSION))
+    define get_sub_header_list
+    $(strip \
+    	$(call get_sub_file_list,$(1),*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
+    )
+    endef
+else
+    define get_sub_header_list
+    $(strip \
+    	$(call get_sub_file_list,$(1),*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
+    	$(call get_sub_file_list,$(1),*.$(patsubst .%,%,$(CONFIG_CXX_HEADER_EXTENSION))) \
+    )
+    endef
+endif
+
+
+#
+# Recursively get a list of files
+# 
+# Warning: Recursively obtaining a list of all files is not recommended. Use only when absolutely necessary.
+#
+# Usage: $(call wildcard_r,<path>,<pattern>)
+# Ex.:   $(call wildcard_r,.,*.cpp)
+define wildcard_r
+$(wildcard $(1)/$(2)) $(foreach dir,$(wildcard $(1)/*),$(call wildcard_r,$(dir),$(2)))
+endef
+
+#
+# Recursively get a list of files in LOCAL_PATH/<path>
+#
+# Warning: Recursively obtaining a list of all files is not recommended. Use only when absolutely necessary.
+#
+# Usage: $(call get_sub_file_list_r,<path>,<pattern>)
+# Ex.:   $(call get_sub_file_list_r,sub_dir,*.cpp)
+define get_sub_file_list_r
 $(strip \
-	$(call get_sub_file_list,$(1),*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
-	$(call get_sub_file_list,$(1),*.$(patsubst .%,%,$(CONFIG_CXX_HEADER_EXTENSION))) \
+	$(patsubst $(LOCAL_PATH)/%,%,
+		$(if $(LOCAL_PATH), \
+			$(call wildcard_r,$(LOCAL_PATH)/$(1),$(2)), \
+			$(error LOCAL_PATH must be set before call get_sub_file_list_r) \
+		) \
+	) \
 )
 endef
 
-# 하위 디렉토리 목록을 얻어옴
-# $(call get_sub_directories,<경로>)
+#
+# Get a list of c/c++ files in LOCAL_PATH/<path>
+#
+# Warning: Recursively obtaining a list of all files is not recommended. Use only when absolutely necessary.
+#
+# Usage: $(call get_sub_source_list_r,<path>)
+# Ex.:   $(call get_sub_source_list_r,sub_dir)
+define get_sub_source_list_r
+$(strip \
+	$(call get_sub_file_list_r,$(1),*.$(patsubst .%,%,$(CONFIG_C_EXTENSION))) \
+	$(call get_sub_file_list_r,$(1),*.$(patsubst .%,%,$(CONFIG_CXX_EXTENSION))) \
+)
+endef
+
+#
+# Get a list of header files in LOCAL_PATH/<path>
+#
+# Warning: Recursively obtaining a list of all files is not recommended. Use only when absolutely necessary.
+#
+# Usage: $(call get_sub_header_list_r,<path>)
+# Ex.:   $(call get_sub_header_list_r,sub_dir)
+ifeq ($(CONFIG_C_HEADER_EXTENSION),$(CONFIG_CXX_HEADER_EXTENSION))
+    define get_sub_header_list_r
+    $(strip \
+    	$(call get_sub_file_list_r,$(1),*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
+    )
+    endef
+else
+    define get_sub_header_list_r
+    $(strip \
+    	$(call get_sub_file_list_r,$(1),*.$(patsubst .%,%,$(CONFIG_C_HEADER_EXTENSION))) \
+    	$(call get_sub_file_list_r,$(1),*.$(patsubst .%,%,$(CONFIG_CXX_HEADER_EXTENSION))) \
+    )
+    endef
+endif
+
+#
+# Get a list of sub directories in LOCAL_PATH
+#
+# Usage: $(call get_sub_directories,<path>)
+# Ex.:   $(call get_sub_directories,.)
 define get_sub_directories
 $(strip \
 	$(patsubst %/,%,${sort ${dir ${wildcard $(1)/*/*}}}) \
