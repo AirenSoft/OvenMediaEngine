9bde51a871ef13a0ed45db6c0a79fdaed0b4a669
Dimiden <dimiden@airensoft.com>
2020-10-30 18:21:47 +0900

    Implementing /v1/vhosts/* API

--- src/projects/api_server/controllers/v1/vhosts/vhosts_controller.cpp
+++ src/projects/api_server/controllers/v1/vhosts/vhosts_controller.cpp
@@ -27,12 +27,121 @@ namespace api
 
 		ApiResponse VHostsController::OnGetVhostList(const std::shared_ptr<HttpClient> &client)
 		{
-			auto vhost_list_config = cfg::ConfigManager::GetInstance()->GetServer()->GetVirtualHostList();
+			const auto &vhost_list_config = cfg::ConfigManager::GetInstance()->GetServer()->GetVirtualHostList();
 			Json::Value response(Json::ValueType::arrayValue);
 
 			for (const auto &vhost : vhost_list_config)
 			{
 				response.append(vhost.GetName().CStr());
+			}
+
+			return response;
+		}
+
+		void SetOptionalString(Json::Value &object, const char *key, const ov::String &value)
+		{
+			if (value.IsEmpty() == false)
+			{
+				object[key] = value.CStr();
+			}
+		}
+
+		Json::Value FillVhost(const cfg::VirtualHost &vhost)
+		{
+			Json::Value response;
+
+			// Field: String name
+			response["name"] = vhost.GetName().CStr();
+
+			// Field: [Optional] Host host
+			const auto &host_config = vhost.GetHost();
+			if (host_config.IsParsed())
+			{
+				Json::Value &host = response["host"];
+
+				// Field: List<String> names
+				Json::Value &names = host["names"];
+				for (const auto &name_config : host_config.GetNameList())
+				{
+					names.append(name_config.GetName().CStr());
+				}
+
+				// Field: [Optional] Tls tls
+				const auto &tls_config = host_config.GetTls();
+				if (tls_config.IsParsed())
+				{
+					Json::Value &tls = host["tls"];
+
+					// Field: String certPath
+					tls["certPath"] = tls_config.GetCertPath().CStr();
+					// Field: String keyPath
+					tls["keyPath"] = tls_config.GetKeyPath().CStr();
+					// Field: [Optional] String chainCertPath
+					SetOptionalString(tls, "chainCertPath", tls_config.GetChainCertPath());
+				}
+			}
+
+			// Field: [Optional] SignedPolicy signedPolicy
+			const auto &signed_policy_config = vhost.GetSignedPolicy();
+			if (signed_policy_config.IsParsed())
+			{
+				Json::Value &signed_policy = response["signedPolicy"];
+
+				// Field: String policyQueryKey
+				signed_policy["policyQueryKey"] = signed_policy_config.GetPolicyQueryKey().CStr();
+				// Field: String signatureQueryKey
+				signed_policy["signatureQueryKey"] = signed_policy_config.GetSignatureQueryKey().CStr();
+				// Field: String secretKey
+				signed_policy["secretKey"] = signed_policy_config.GetSecretKey().CStr();
+			}
+
+			// Field: [Optional] SignedToken signedToken
+			const auto &signed_token_config = vhost.GetSignedToken();
+			if (signed_token_config.IsParsed())
+			{
+				Json::Value &signed_token = response["signedToken"];
+
+				// Field: String cryptoKey
+				signed_token["cryptoKey"] = signed_token_config.GetCryptoKey().CStr();
+				// Field: String queryStringKey
+				signed_token["queryStringKey"] = signed_token_config.GetQueryStringKey().CStr();
+			}
+
+			// Field: [Optional] List<OriginMap> originMaps
+			const auto &origins_config = vhost.GetOrigins();
+			if (origins_config.IsParsed())
+			{
+				Json::Value &origin_maps = response["originMaps"];
+
+				for (auto &origin_config : origins_config.GetOriginList())
+				{
+					Json::Value origin;
+
+					// Field: String location
+					origin["location"] = origin_config.GetLocation().CStr();
+
+					// Field: Pass pass
+					const auto &pass_config = origin_config.GetPass();
+					{
+						Json::Value &pass = origin["pass"];
+
+						// Field: String scheme
+						pass["scheme"] = pass_config.GetScheme().CStr();
+
+						// Field: List<String> urls
+						const auto &url_list_config = pass_config.GetUrlList();
+						{
+							Json::Value &url_list = pass["urls"];
+
+							for (auto &url : url_list_config)
+							{
+								url_list.append(url.GetUrl().CStr());
+							}
+						}
+					}
+
+					origin_maps.append(origin);
+				}
 			}
 
 			return response;
@@ -44,16 +153,13 @@ namespace api
 			auto &match_result = client->GetRequest()->GetMatchResult();
 			auto vhost_name = match_result.GetNamedGroup("vhost_name");
 
-			auto vhost_list_config = cfg::ConfigManager::GetInstance()->GetServer()->GetVirtualHostList();
+			const auto &vhost_list_config = cfg::ConfigManager::GetInstance()->GetServer()->GetVirtualHostList();
 
 			for (const auto &vhost : vhost_list_config)
 			{
 				if (vhost_name == vhost.GetName().CStr())
 				{
-					// TODO(dimiden): Fill this information
-					Json::Value response(Json::ValueType::objectValue);
-					response["name"] = vhost.GetName().CStr();
-					return response;
+					return FillVhost(vhost);
 				}
 			}
 
