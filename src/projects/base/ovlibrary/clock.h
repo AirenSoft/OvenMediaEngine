fc5bd19bdc8ce71b82b680a8c0e37de1f816fb68
getroot <getroot@airensoft.com>
2020-11-02 13:00:01 +0900

    Change localtime function to thread-safe

--- src/projects/base/ovlibrary/clock.h
+++ src/projects/base/ovlibrary/clock.h
@@ -24,11 +24,16 @@ namespace ov
 			auto tp_sec = std::chrono::system_clock::from_time_t(ttime_t);
 			auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - tp_sec);
 
-			std::tm * ttm = localtime(&ttime_t);
+			struct tm lt;
+			if(localtime_r(&ttime_t, &lt) == nullptr)
+			{
+				return "";
+			}
+
 			char date_time_format[] = "%Y.%m.%d-%H.%M.%S";
 			char time_str[] = "yyyy.mm.dd.HH-MM.SS.fff";
 
-			strftime(time_str, strlen(time_str), date_time_format, ttm);
+			strftime(time_str, strlen(time_str), date_time_format, &lt);
 
 			ov::String result;
 			result.AppendFormat("%s.%u", time_str, ms.count());
