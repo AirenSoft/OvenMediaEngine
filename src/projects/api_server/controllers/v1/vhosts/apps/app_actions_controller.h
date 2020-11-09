6d80a4f3aa205ad8edd8a1d9c8814e316b41d3ba
Dimiden <dimiden@airensoft.com>
2020-11-09 21:28:12 +0900

    Organized source code

--- src/projects/api_server/controllers/v1/vhosts/apps/app_actions_controller.h
+++ src/projects/api_server/controllers/v1/vhosts/apps/app_actions_controller.h
@@ -20,20 +20,29 @@ namespace api
 			void PrepareHandlers() override;
 
 		protected:
+			ApiResponse OnGetRecords(const std::shared_ptr<HttpClient> &client,
+									 const std::shared_ptr<mon::HostMetrics> &vhost,
+									 const std::shared_ptr<mon::ApplicationMetrics> &app);
+			ApiResponse OnPostStartRecord(const std::shared_ptr<HttpClient> &client, const Json::Value &request_body,
+										  const std::shared_ptr<mon::HostMetrics> &vhost,
+										  const std::shared_ptr<mon::ApplicationMetrics> &app);
+			ApiResponse OnPostStopRecord(const std::shared_ptr<HttpClient> &client, const Json::Value &request_body,
+										 const std::shared_ptr<mon::HostMetrics> &vhost,
+										 const std::shared_ptr<mon::ApplicationMetrics> &app);
 
-			ApiResponse OnGetRecords(const std::shared_ptr<HttpClient> &clnt);
-			ApiResponse OnPostStartRecord(const std::shared_ptr<HttpClient> &clnt, const Json::Value &request_body);
-			ApiResponse OnPostStopRecord(const std::shared_ptr<HttpClient> &clnt, const Json::Value &request_body);
-
-			ApiResponse OnGetPushes(const std::shared_ptr<HttpClient> &clnt);
-			ApiResponse OnPostStartPush(const std::shared_ptr<HttpClient> &clnt, const Json::Value &request_body);
-			ApiResponse OnPostStopPush(const std::shared_ptr<HttpClient> &clnt, const Json::Value &request_body);
+			ApiResponse OnGetPushes(const std::shared_ptr<HttpClient> &client);
+			ApiResponse OnPostStartPush(const std::shared_ptr<HttpClient> &client, const Json::Value &request_body);
+			ApiResponse OnPostStopPush(const std::shared_ptr<HttpClient> &client, const Json::Value &request_body);
 
 			// POST /v1/vhosts/<vhost_name>/apps/<app_name>:<action>
-			ApiResponse OnPostDummyAction(const std::shared_ptr<HttpClient> &clnt, const Json::Value &request_body);
+			ApiResponse OnPostDummyAction(const std::shared_ptr<HttpClient> &client, const Json::Value &request_body,
+										  const std::shared_ptr<mon::HostMetrics> &vhost,
+										  const std::shared_ptr<mon::ApplicationMetrics> &app);
 
 			// GET /v1/vhosts/<vhost_name>/apps/<app_name>:<action>
-			ApiResponse OnGetDummyAction(const std::shared_ptr<HttpClient> &clnt);
+			ApiResponse OnGetDummyAction(const std::shared_ptr<HttpClient> &client,
+										 const std::shared_ptr<mon::HostMetrics> &vhost,
+										 const std::shared_ptr<mon::ApplicationMetrics> &app);
 		};
 	}  // namespace v1
 }  // namespace api
