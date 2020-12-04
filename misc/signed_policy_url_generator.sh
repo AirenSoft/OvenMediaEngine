adeb6fd88dfc3f66436b04316776dd35107a4e2f
alexufo <serebniti@gmail.com>
2020-12-04 16:32:12 +0300

    fix example in generator

--- misc/signed_policy_url_generator.sh
+++ misc/signed_policy_url_generator.sh
@@ -12,7 +12,7 @@ then
 	echo ""
 	echo "Example:"
 	echo "    $0 ome_is_the_best ws://host:3333/app/stream signature policy '{\"url_expire\":1604377520178}'"
-	echo "    => ws://host:3333/app/stream?policy=e3BvbGljeV9leHBpcmU6MTYwNDM3NzUyMDE3OH0&signature=WTgJM2CZZ5WGGsnwyAIrlzZNA_Q"
+	echo "    => ws://host:3333/app/stream?policy=e1widXJsX2V4cGlyZVwiOjE2MDQzNzc1MjAxNzh9&signature=rXfU1z1ynBTfK-q6_HM_I9fPzRs"
 
 	exit
 fi
