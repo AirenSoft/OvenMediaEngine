4a00dafab8347eca92638f9e64a2e979bb0b68c1
getroot <getroot@airensoft.com>
2021-01-07 23:20:59 +0900

    Fixed DASH output bytes

--- src/projects/publishers/segment/dash/dash_stream_server.cpp
+++ src/projects/publishers/segment/dash/dash_stream_server.cpp
@@ -8,8 +8,11 @@
 //==============================================================================
 #include "dash_stream_server.h"
 
+#include <monitoring/monitoring.h>
+
 #include <publishers/segment/segment_stream/packetizer/packetizer_define.h>
 
+#include "../segment_publisher.h"
 #include "dash_define.h"
 #include "dash_private.h"
 
@@ -41,10 +44,19 @@ HttpConnection DashStreamServer::ProcessPlayListRequest(const std::shared_ptr<Ht
 	auto response = client->GetResponse();
 
 	ov::String play_list;
+	std::shared_ptr<info::Stream> stream_info;
 
 	auto item = std::find_if(_observers.begin(), _observers.end(),
-							 [client, request_info, &play_list](std::shared_ptr<SegmentStreamObserver> &observer) -> bool {
-								 return observer->OnPlayListRequest(client, request_info, play_list);
+							 [client, request_info, &play_list, &stream_info](std::shared_ptr<SegmentStreamObserver> &observer) -> bool {
+								
+								std::shared_ptr<SegmentPublisher> publisher = std::static_pointer_cast<SegmentPublisher>(observer);
+								if(observer->OnPlayListRequest(client, request_info, play_list))
+							 	{
+									stream_info = publisher->GetStreamAs<info::Stream>(request_info.vhost_app_name, request_info.stream_name);
+									return true;
+								}
+
+								return false;
 							 });
 
 	if ((item == _observers.end()))
@@ -69,7 +81,16 @@ HttpConnection DashStreamServer::ProcessPlayListRequest(const std::shared_ptr<Ht
 	response->SetHeader("Expires", "0");
 
 	response->AppendString(play_list);
-	response->Response();
+	auto sent_bytes = response->Response();
+
+	if (stream_info != nullptr)
+	{
+		auto stream_metric = StreamMetrics(*stream_info);
+		if (stream_metric != nullptr)
+		{
+			stream_metric->IncreaseBytesOut(PublisherType::Dash, sent_bytes);
+		}
+	}
 
 	return HttpConnection::Closed;
 }
@@ -81,9 +102,13 @@ HttpConnection DashStreamServer::ProcessSegmentRequest(const std::shared_ptr<Htt
 	auto response = client->GetResponse();
 
 	std::shared_ptr<const SegmentItem> segment = nullptr;
+	std::shared_ptr<info::Stream> stream_info;
 
 	auto item = std::find_if(_observers.begin(), _observers.end(),
-							 [client, request_info, &segment](auto &observer) -> bool {
+							 [client, request_info, &segment, &stream_info](auto &observer) -> bool {
+								 auto publisher = std::static_pointer_cast<SegmentPublisher>(observer);
+								 auto stream = publisher->GetStream(request_info.vhost_app_name, request_info.stream_name);
+								 stream_info = std::static_pointer_cast<info::Stream>(stream);
 								 return observer->OnSegmentRequest(client, request_info, segment);
 							 });
 
@@ -99,7 +124,16 @@ HttpConnection DashStreamServer::ProcessSegmentRequest(const std::shared_ptr<Htt
 	// Set HTTP header
 	response->SetHeader("Content-Type", (segment->type == SegmentDataType::Video) ? "video/mp4" : "audio/mp4");
 	response->AppendData(segment->data);
-	response->Response();
+	auto sent_bytes = response->Response();
+
+	if (stream_info != nullptr)
+	{
+		auto stream_metric = StreamMetrics(*stream_info);
+		if (stream_metric != nullptr)
+		{
+			stream_metric->IncreaseBytesOut(PublisherType::Dash, sent_bytes);
+		}
+	}
 
 	return HttpConnection::Closed;
 }
