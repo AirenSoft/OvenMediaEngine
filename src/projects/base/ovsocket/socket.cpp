97e865f3f73cb1f2617192457b3dbdbd864c4af5
getroot <getroot@airensoft.com>
2021-05-04 14:31:17 +0900

    Fixed SRT blocking mode change problem

--- src/projects/base/ovsocket/socket.cpp
+++ src/projects/base/ovsocket/socket.cpp
@@ -234,9 +234,7 @@ namespace ov
 			}
 
 			case SocketType::Srt:
-				if (
-					SetSockOpt(SRTO_RCVSYN, mode != BlockingMode::Blocking) ||
-					SetSockOpt(SRTO_SNDSYN, mode != BlockingMode::Blocking))
+				if (SetSockOpt(SRTO_RCVSYN, mode != BlockingMode::Blocking) == false || SetSockOpt(SRTO_SNDSYN, mode != BlockingMode::Blocking) == false)
 				{
 					logae("Could not set flags to SRT socket %d: %s", GetNativeHandle(), Error::CreateErrorFromSrt()->ToString().CStr());
 					return false;
