d10c46e0f5ba1cca2eba24294097a75166167aea
Dimiden <dimiden@airensoft.com>
2021-11-23 18:59:32 +0900

    Added RTMP packet dump feature to debug

--- src/projects/providers/rtmp/rtmp_provider.cpp
+++ src/projects/providers/rtmp/rtmp_provider.cpp
@@ -20,8 +20,29 @@
 
 namespace pvd
 {
+#if DEBUG
+	static bool dump_packet = false;
+	static void DumpDataToFile(const std::shared_ptr<ov::Socket> &remote,
+							   const ov::SocketAddress &address,
+							   const std::shared_ptr<const ov::Data> &data)
+	{
+		if (dump_packet)
+		{
+			auto file_name = ov::String::FormatString("%s_%s",
+													  ov::Time::MakeUtcSecond().CStr(),
+													  remote->GetRemoteAddress()->ToString().Replace(":", "_").CStr());
+
+			ov::DumpToFile(ov::PathManager::Combine(ov::PathManager::GetAppPath("dump/rtmp"), file_name), data, true);
+		}
+	}
+#endif	// DEBUG
+
 	std::shared_ptr<RtmpProvider> RtmpProvider::Create(const cfg::Server &server_config, const std::shared_ptr<MediaRouteInterface> &router)
 	{
+#if DEBUG
+		dump_packet = ov::Converter::ToBool(std::getenv("OME_DUMP_RTMP"));
+#endif	// DEBUG
+
 		auto provider = std::make_shared<RtmpProvider>(server_config, router);
 		if (!provider->Start())
 		{
@@ -123,6 +144,10 @@ namespace pvd
 						const ov::SocketAddress &address,
 						const std::shared_ptr<const ov::Data> &data)
 	{
+#if DEBUG
+		DumpDataToFile(remote, address, data);
+#endif	// DEBUG
+
 		PushProvider::OnDataReceived(remote->GetNativeHandle(), data);
 	}
 
