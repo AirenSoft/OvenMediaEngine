eb4f21a8afe0e71599c3fe5420d45fc67c5460a1
Keukhan <soulk@airensoft.com>
2022-01-13 23:21:27 +0900

    disable openh264 prebuilt library download script

--- misc/prerequisites.sh
+++ misc/prerequisites.sh
@@ -107,22 +107,22 @@ install_libopenh264()
     # We send an audit to Cisco.
     # "OpenH264 Video Codec provided by Cisco Systems, Inc."
 
-    KERNEL=$(uname -s)
-    ARCH=$(uname -m)
+    # KERNEL=$(uname -s)
+    # ARCH=$(uname -m)
 
-    if  [ "${KERNEL}" == "Linux" ] && [ "${ARCH}" == "x86" ]; then
-        PLATFORM="linux32"
-    elif [ "${KERNEL}" == "Linux" ] && [ "${ARCH}" == "x86_64" ]; then
-        PLATFORM="linux64"
-    else
-        return
-    fi 
+    # if  [ "${KERNEL}" == "Linux" ] && [ "${ARCH}" == "x86" ]; then
+    #     PLATFORM="linux32"
+    # elif [ "${KERNEL}" == "Linux" ] && [ "${ARCH}" == "x86_64" ]; then
+    #     PLATFORM="linux64"
+    # else
+    #     return
+    # fi 
 
-    (cd ${PREFIX}/lib && \
-    FILENAME=libopenh264-${OPENH264_VERSION}-${PLATFORM}.6.so && \
-    sudo curl -O http://ciscobinary.openh264.org/${FILENAME}.bz2 && \
-    sudo bunzip2 -f ${FILENAME}.bz2 && \
-    sudo mv ${FILENAME} libopenh264.so.${OPENH264_VERSION} ) || fail_exit "prebuilt_openh264"
+    # (cd ${PREFIX}/lib && \
+    # FILENAME=libopenh264-${OPENH264_VERSION}-${PLATFORM}.6.so && \
+    # sudo curl -O http://ciscobinary.openh264.org/${FILENAME}.bz2 && \
+    # sudo bunzip2 -f ${FILENAME}.bz2 && \
+    # sudo mv ${FILENAME} libopenh264.so.${OPENH264_VERSION} ) || fail_exit "prebuilt_openh264"
 }
 
 install_libvpx()
