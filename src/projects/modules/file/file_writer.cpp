9059c6332fb740406a1530bec90cc1bb369a7793
Keukhan <soulk@airensot.com>
2022-04-20 23:17:31 +0900

    Fixed the problem that A-V sync was broken due to start_time in file recording

--- src/projects/modules/file/file_writer.cpp
+++ src/projects/modules/file/file_writer.cpp
@@ -56,10 +56,7 @@ std::shared_ptr<FileWriter> FileWriter::Create()
 FileWriter::FileWriter()
 	: _format_context(nullptr)
 {
-	// av_log_set_callback(FileWriter::FFmpegLog);
-	// av_log_set_level(AV_LOG_TRACE);
-
-	_start_timestamp = -1L;
+	_start_time = -1L;
 	_timestamp_recalc_mode = TIMESTAMP_STARTZERO_MODE;
 }
 
@@ -77,7 +74,6 @@ int FileWriter::GetTimestampRecalcMode()
 {
 	return _timestamp_recalc_mode;
 }
-
 
 bool FileWriter::SetPath(const ov::String path, const ov::String format)
 {
@@ -136,7 +132,7 @@ bool FileWriter::Start()
 
 	AVDictionary *options = nullptr;
 
-	_start_timestamp = -1LL;
+	_start_time = -1LL;
 
 	if (!(_format_context->oformat->flags & AVFMT_NOFILE))
 	{
@@ -188,7 +184,7 @@ bool FileWriter::Stop()
 
 		_format_context = nullptr;
 
-		if(chmod(path.CStr(), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) != 0)
+		if (chmod(path.CStr(), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) != 0)
 		{
 			logtw("Could not change permission. path(%s)", path.CStr());
 		}
@@ -197,7 +193,7 @@ bool FileWriter::Stop()
 	return true;
 }
 
-bool FileWriter::AddTrack(cmn::MediaType media_type, int32_t track_id, std::shared_ptr<FileTrackInfo> track_info)
+bool FileWriter::AddTrack(cmn::MediaType media_type, int32_t track_id, std::shared_ptr<FileTrackInfo> track)
 {
 	std::lock_guard<std::shared_mutex> mlock(_lock);
 
@@ -210,30 +206,30 @@ bool FileWriter::AddTrack(cmn::MediaType media_type, int32_t track_id, std::shar
 			AVCodecParameters *codecpar = stream->codecpar;
 
 			codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
-			codecpar->codec_id = (track_info->GetCodecId() == cmn::MediaCodecId::H264) ? AV_CODEC_ID_H264 : (track_info->GetCodecId() == cmn::MediaCodecId::H265) ? AV_CODEC_ID_H265
-																										: (track_info->GetCodecId() == cmn::MediaCodecId::Vp8)	  ? AV_CODEC_ID_VP8
-																										: (track_info->GetCodecId() == cmn::MediaCodecId::Vp9)	  ? AV_CODEC_ID_VP9
+			codecpar->codec_id = (track->GetCodecId() == cmn::MediaCodecId::H264) ? AV_CODEC_ID_H264 : (track->GetCodecId() == cmn::MediaCodecId::H265) ? AV_CODEC_ID_H265
+																										: (track->GetCodecId() == cmn::MediaCodecId::Vp8)	  ? AV_CODEC_ID_VP8
+																										: (track->GetCodecId() == cmn::MediaCodecId::Vp9)	  ? AV_CODEC_ID_VP9
 																																								  : AV_CODEC_ID_NONE;
 
-			codecpar->bit_rate = track_info->GetBitrate();
-			codecpar->width = track_info->GetWidth();
-			codecpar->height = track_info->GetHeight();
+			codecpar->bit_rate = track->GetBitrate();
+			codecpar->width = track->GetWidth();
+			codecpar->height = track->GetHeight();
 			codecpar->sample_aspect_ratio = AVRational{1, 1};
 			codecpar->codec_tag = 0;
 
 			// set extradata for avc_decoder_configuration_record
-			if (track_info->GetExtradata() != nullptr)
+			if (track->GetExtradata() != nullptr)
 			{
-				codecpar->extradata_size = track_info->GetExtradata()->GetLength();
+				codecpar->extradata_size = track->GetExtradata()->GetLength();
 				codecpar->extradata = (uint8_t *)av_malloc(codecpar->extradata_size + AV_INPUT_BUFFER_PADDING_SIZE);
 				memset(codecpar->extradata, 0, codecpar->extradata_size + AV_INPUT_BUFFER_PADDING_SIZE);
-				memcpy(codecpar->extradata, track_info->GetExtradata()->GetDataAs<uint8_t>(), codecpar->extradata_size);
+				memcpy(codecpar->extradata, track->GetExtradata()->GetDataAs<uint8_t>(), codecpar->extradata_size);
 			}
 
-			stream->time_base = AVRational{track_info->GetTimeBase().GetNum(), track_info->GetTimeBase().GetDen()};
+			stream->time_base = AVRational{track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen()};
 
-			_track_map[track_id] = stream->index;
-			_trackinfo_map[track_id] = track_info;
+			_track_to_avstream[track_id] = stream->index;
+			_tracks[track_id] = track;
 		}
 		break;
 
@@ -242,30 +238,30 @@ bool FileWriter::AddTrack(cmn::MediaType media_type, int32_t track_id, std::shar
 			AVCodecParameters *codecpar = stream->codecpar;
 
 			codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
-			codecpar->codec_id = (track_info->GetCodecId() == cmn::MediaCodecId::Aac) ? AV_CODEC_ID_AAC : (track_info->GetCodecId() == cmn::MediaCodecId::Mp3) ? AV_CODEC_ID_MP3
-																									  : (track_info->GetCodecId() == cmn::MediaCodecId::Opus)  ? AV_CODEC_ID_OPUS
+			codecpar->codec_id = (track->GetCodecId() == cmn::MediaCodecId::Aac) ? AV_CODEC_ID_AAC : (track->GetCodecId() == cmn::MediaCodecId::Mp3) ? AV_CODEC_ID_MP3
+																									  : (track->GetCodecId() == cmn::MediaCodecId::Opus)  ? AV_CODEC_ID_OPUS
 																																							   : AV_CODEC_ID_NONE;
-			codecpar->bit_rate = track_info->GetBitrate();
-			codecpar->channels = static_cast<int>(track_info->GetChannel().GetCounts());
-			codecpar->channel_layout = (track_info->GetChannel().GetLayout() == cmn::AudioChannel::Layout::LayoutMono) ? AV_CH_LAYOUT_MONO : (track_info->GetChannel().GetLayout() == cmn::AudioChannel::Layout::LayoutStereo) ? AV_CH_LAYOUT_STEREO
+			codecpar->bit_rate = track->GetBitrate();
+			codecpar->channels = static_cast<int>(track->GetChannel().GetCounts());
+			codecpar->channel_layout = (track->GetChannel().GetLayout() == cmn::AudioChannel::Layout::LayoutMono) ? AV_CH_LAYOUT_MONO : (track->GetChannel().GetLayout() == cmn::AudioChannel::Layout::LayoutStereo) ? AV_CH_LAYOUT_STEREO
 																																																							   : 0;	 // <- Unknown
-			codecpar->sample_rate = track_info->GetSample().GetRateNum();
-			codecpar->frame_size = 1024;  // TODO: Need to Frame Size
+			codecpar->sample_rate = track->GetSample().GetRateNum();
+			codecpar->frame_size = 1024;  // Default Frame Size(1024)
 			codecpar->codec_tag = 0;
 
 			// set extradata for aac_specific_config
-			if (track_info->GetExtradata() != nullptr)
+			if (track->GetExtradata() != nullptr)
 			{
-				codecpar->extradata_size = track_info->GetExtradata()->GetLength();
+				codecpar->extradata_size = track->GetExtradata()->GetLength();
 				codecpar->extradata = (uint8_t *)av_malloc(codecpar->extradata_size + AV_INPUT_BUFFER_PADDING_SIZE);
 				memset(codecpar->extradata, 0, codecpar->extradata_size + AV_INPUT_BUFFER_PADDING_SIZE);
-				memcpy(codecpar->extradata, track_info->GetExtradata()->GetDataAs<uint8_t>(), codecpar->extradata_size);
+				memcpy(codecpar->extradata, track->GetExtradata()->GetDataAs<uint8_t>(), codecpar->extradata_size);
 			}
 
-			stream->time_base = AVRational{track_info->GetTimeBase().GetNum(), track_info->GetTimeBase().GetDen()};
+			stream->time_base = AVRational{track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen()};
 
-			_track_map[track_id] = stream->index;
-			_trackinfo_map[track_id] = track_info;
+			_track_to_avstream[track_id] = stream->index;
+			_tracks[track_id] = track;
 		}
 		break;
 
@@ -290,64 +286,67 @@ bool FileWriter::PutData(int32_t track_id, int64_t pts, int64_t dts, MediaPacket
 	if (_format_context == nullptr)
 		return false;
 
-	// Find AVStream and Index;
-	int stream_index = 0;
-
-	auto iter = _track_map.find(track_id);
-	if (iter == _track_map.end())
+	auto iter = _track_to_avstream.find(track_id);
+	if (iter == _track_to_avstream.end())
 	{
-		// logtw("There is no track id %d", track_id);
-
-		// Without a track, it's not an error. Ignore.
+		// If track_id is not on the recording target list, drop it without errors.
 		return true;
 	}
-	stream_index = iter->second;
 
+	int32_t stream_index = iter->second;
+	
 	AVStream *stream = _format_context->streams[stream_index];
 	if (stream == nullptr)
 	{
 		logtw("There is no stream");
 		return false;
 	}
+	
+	// Find TrackInfo
+	auto track = _tracks[track_id];
 
-	// Find Ouput Track Info
-	auto track_info = _trackinfo_map[track_id];
 
-	if(GetTimestampRecalcMode() == TIMESTAMP_STARTZERO_MODE)
+	if (GetTimestampRecalcMode() == TIMESTAMP_STARTZERO_MODE)
 	{
-		if (_start_timestamp == -1LL)
+		if (_start_time == -1LL)
 		{
-			_start_timestamp = pts;
+			// Convert media track's timebase to standard timebase
+			_start_time = av_rescale_q(pts, AVRational{track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen()}, AV_TIME_BASE_Q);
 		}
 	}
-	else if(GetTimestampRecalcMode() == TIMESTAMP_PASSTHROUGH_MODE)
+	else if (GetTimestampRecalcMode() == TIMESTAMP_PASSTHROUGH_MODE)
 	{
-		_start_timestamp = 0LL;
+		_start_time = 0LL;
 	}
 
+	// / Convert standard timebase to media track's timebase
+	int64_t start_time = av_rescale_q(_start_time, AV_TIME_BASE_Q, AVRational{track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen()});
 
-	// Make avpacket
 	AVPacket av_packet = {0};
-
 	av_packet.stream_index = stream_index;
 	av_packet.flags = (flag == MediaPacketFlag::Key) ? AV_PKT_FLAG_KEY : 0;
-	av_packet.pts = av_rescale_q(pts - _start_timestamp, AVRational{track_info->GetTimeBase().GetNum(), track_info->GetTimeBase().GetDen()}, stream->time_base);
-	av_packet.dts = av_rescale_q(dts - _start_timestamp, AVRational{track_info->GetTimeBase().GetNum(), track_info->GetTimeBase().GetDen()}, stream->time_base);
+	av_packet.pts = av_rescale_q(pts - start_time, AVRational{track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen()}, stream->time_base);
+	av_packet.dts = av_rescale_q(dts - start_time, AVRational{track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen()}, stream->time_base);
 
-	// TODO: Depending on the extension, the bitstream format should be changed.
+	logtp("%d] %lld / %10lld[%d/%5d] -> %10lld[%d/%5d]",
+		  stream_index, _start_time,
+		  pts, track->GetTimeBase().GetNum(), track->GetTimeBase().GetDen(),
+		  av_packet.pts, stream->time_base.num, stream->time_base.den);
+
+	// Depending on the extension, the bitstream format should be changed.
 	// format(mpegts)
 	// 	- H264 : Passthrough (AnnexB)
 	//  - H265 : Passthrough (AnnexB)
 	//  - AAC : Passthrough (ADTS)
-	//  - OPUS : Passthrough (?)
-	//  - VP8 : Passthrough (unknown name)
+	//  - OPUS : Passthrough 
+	//  - VP8 : Passthrough 
 	// format(mp4)
 	//	- H264 : AVCC
 	//	- AAC : to RA
 	// format(webm)
 	//	- VP8 : Passthrough
 	//	- VP9 : Passthrough
-	//  - OPUS : Passthrough 
+	//  - OPUS : Passthrough
 	uint8_t ADTS_HEADER_LENGTH = 7;
 
 	std::shared_ptr<const ov::Data> cdata = nullptr;
@@ -415,44 +414,6 @@ bool FileWriter::IsWritable()
 	return true;
 }
 
-// FFMPEG DEBUG
-void FileWriter::FFmpegLog(void *ptr, int level, const char *fmt, va_list vl)
-{
-	va_list vl2;
-	char line[1024];
-	static int print_prefix = 1;
-
-	va_copy(vl2, vl);
-	av_log_default_callback(ptr, level, fmt, vl);
-	av_log_format_line(ptr, level, fmt, vl2, line, sizeof(line), &print_prefix);
-	va_end(vl2);
-
-	// if(level >= AV_LOG_DEBUG) return;
-
-	switch (level)
-	{
-		case AV_LOG_QUIET:
-		case AV_LOG_DEBUG:
-		case AV_LOG_VERBOSE:
-			logd("FFmpeg", line, vl);
-			break;
-		case AV_LOG_INFO:
-			logi("FFmpeg", line, vl);
-			break;
-		case AV_LOG_WARNING:
-			logw("FFmpeg", line, vl);
-			break;
-		case AV_LOG_ERROR:
-		case AV_LOG_FATAL:
-		case AV_LOG_PANIC:
-			loge("FFmpeg", line, vl);
-			break;
-		case AV_LOG_TRACE:
-			//		log_level = ANDROID_LOG_VERBOSE;
-			break;
-	}
-}
-
 ov::String FileWriter::GetFormatByExtension(ov::String extension, ov::String default_format)
 {
 	if (extension == "mp4")
@@ -463,7 +424,7 @@ ov::String FileWriter::GetFormatByExtension(ov::String extension, ov::String def
 	{
 		return "mpegts";
 	}
-	else if(extension == "webm")
+	else if (extension == "webm")
 	{
 		return "webm";
 	}
