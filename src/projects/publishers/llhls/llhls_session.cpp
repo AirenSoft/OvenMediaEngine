ca0f78723ad19c8c69ed110446c6e9ddf72aafde
getroot <getroot@airensoft.com>
2022-05-13 12:57:32 +0900

    Changed max pending requests log level

--- src/projects/publishers/llhls/llhls_session.cpp
+++ src/projects/publishers/llhls/llhls_session.cpp
@@ -487,7 +487,7 @@ bool LLHlsSession::AddPendingRequest(const std::shared_ptr<http::svr::HttpExchan
 
 	if (_pending_requests.size() > MAX_PENDING_REQUESTS)
 	{
-		logtw("[%s/%s/%u] Too many pending requests (%u)", 
+		logtd("[%s/%s/%u] Too many pending requests (%u)", 
 				GetApplication()->GetName().CStr(),
 				GetStream()->GetName().CStr(),
 				GetId(),
