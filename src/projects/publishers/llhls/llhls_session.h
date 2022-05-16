2113e9bbce676732016a2854c2a66b47f7efb862
getroot <getroot@airensoft.com>
2022-05-16 15:27:12 +0900

    Fixed memory leak of LLHLS

--- src/projects/publishers/llhls/llhls_session.h
+++ src/projects/publishers/llhls/llhls_session.h
@@ -50,7 +50,7 @@ private:
 	void ResponseSegment(const std::shared_ptr<http::svr::HttpExchange> &exchange, const int32_t &track_id, const int64_t &segment_number);
 	void ResponsePartialSegment(const std::shared_ptr<http::svr::HttpExchange> &exchange, const int32_t &track_id, const int64_t &segment_number, const int64_t &partial_number);
 
-	void ResponseData(const std::shared_ptr<http::svr::HttpResponse> &response);
+	void ResponseData(const std::shared_ptr<http::svr::HttpExchange> &exchange);
 
 	void OnPlaylistUpdated(const int32_t &track_id, const int64_t &msn, const int64_t &part);
 
