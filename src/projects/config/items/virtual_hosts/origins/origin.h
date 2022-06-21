4c49b54b901ed5586a2a4a5c77df5beba018a447
Keukhan <soulk@airensot.com>
2022-06-21 14:58:14 +0900

    Change to Persistence terms

--- src/projects/config/items/virtual_hosts/origins/origin.h
+++ src/projects/config/items/virtual_hosts/origins/origin.h
@@ -20,7 +20,7 @@ namespace cfg
 			{
 				CFG_DECLARE_CONST_REF_GETTER_OF(GetLocation, _location)
 				CFG_DECLARE_CONST_REF_GETTER_OF(GetPass, _pass)
-				CFG_DECLARE_CONST_REF_GETTER_OF(IsPersist, _persist)
+				CFG_DECLARE_CONST_REF_GETTER_OF(IsPersistence, _persistence)
 				CFG_DECLARE_CONST_REF_GETTER_OF(IsFailback, _failback)
 				CFG_DECLARE_CONST_REF_GETTER_OF(IsStrictLocation, _strict_location)
 				CFG_DECLARE_CONST_REF_GETTER_OF(IsRelay, _relay)
@@ -30,14 +30,14 @@ namespace cfg
 				{
 					Register("Location", &_location);
 					Register("Pass", &_pass);
-					Register<Optional>("Persist", &_persist);
+					Register<Optional>("Persistence", &_persistence);
 					Register<Optional>("Failback", &_failback);
 					Register<Optional>("StrictLocation", &_strict_location);
 					Register<Optional>("Relay", &_relay);
 				}
 				ov::String _location;
 				Pass _pass;
-				bool _persist = false;
+				bool _persistence = false;
 				bool _failback = false;
 				bool _strict_location = false;
 				bool _relay = true;
