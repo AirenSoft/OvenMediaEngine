b7455ff36a2f9d3606054cf370b55c25644037d2
getroot <getroot@airensoft.com>
2022-07-20 10:17:55 +0900

    Release stream session queue

--- src/projects/base/publisher/stream.cpp
+++ src/projects/base/publisher/stream.cpp
@@ -45,6 +45,8 @@ namespace pub
 		_stop_thread_flag = true;
 		// Generate Event
 		_packet_queue.Stop();
+		_session_message_queue.Stop();
+		
 		_queue_event.Notify();
 		if(_worker_thread.joinable())
 		{
