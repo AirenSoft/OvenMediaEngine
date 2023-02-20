5c27edff27587b2918770540c7d744fd4d9b7f66
Dimiden <dimiden@airensoft.com>
2023-02-20 21:17:18 +0900

    Apply formatter

--- src/projects/publishers/webrtc/webrtc_publisher.h
+++ src/projects/publishers/webrtc/webrtc_publisher.h
@@ -8,17 +8,18 @@
 //==============================================================================
 #pragma once
 
-#include "base/common_types.h"
-#include "base/publisher/publisher.h"
-#include "base/mediarouter/mediarouter_application_interface.h"
-#include "base/ovlibrary/message_thread.h"
-#include "base/ovlibrary/delay_queue.h"
-#include "rtc_application.h"
 #include <orchestrator/orchestrator.h>
 
+#include "base/common_types.h"
+#include "base/mediarouter/mediarouter_application_interface.h"
+#include "base/ovlibrary/delay_queue.h"
+#include "base/ovlibrary/message_thread.h"
+#include "base/publisher/publisher.h"
+#include "rtc_application.h"
+
 class WebRtcPublisher : public pub::Publisher,
-                        public IcePortObserver,
-                        public RtcSignallingObserver,
+						public IcePortObserver,
+						public RtcSignallingObserver,
 						public ov::MessageThreadObserver<std::shared_ptr<ov::CommonMessage>>
 {
 public:
@@ -39,28 +40,28 @@ public:
 
 	// SignallingObserver Implementation
 	std::shared_ptr<const SessionDescription> OnRequestOffer(const std::shared_ptr<http::svr::ws::WebSocketSession> &ws_session,
-													   const info::VHostAppName &vhost_app_name, const ov::String &host_name, const ov::String &stream_name,
-													   std::vector<RtcIceCandidate> *ice_candidates, bool &tcp_relay) override;
+															 const info::VHostAppName &vhost_app_name, const ov::String &host_name, const ov::String &stream_name,
+															 std::vector<RtcIceCandidate> *ice_candidates, bool &tcp_relay) override;
 	bool OnAddRemoteDescription(const std::shared_ptr<http::svr::ws::WebSocketSession> &ws_session,
 								const info::VHostAppName &vhost_app_name, const ov::String &host_name, const ov::String &stream_name,
 								const std::shared_ptr<const SessionDescription> &offer_sdp,
 								const std::shared_ptr<const SessionDescription> &peer_sdp) override;
 
 	bool OnChangeRendition(const std::shared_ptr<http::svr::ws::WebSocketSession> &ws_session,
-							bool change_rendition, const ov::String &rendition_name, bool change_auto, bool &auto_abr,
-							const std::shared_ptr<const SessionDescription> &offer_sdp,
-							const std::shared_ptr<const SessionDescription> &peer_sdp) override;
+						   bool change_rendition, const ov::String &rendition_name, bool change_auto, bool &auto_abr,
+						   const std::shared_ptr<const SessionDescription> &offer_sdp,
+						   const std::shared_ptr<const SessionDescription> &peer_sdp) override;
 
 	bool OnIceCandidate(const std::shared_ptr<http::svr::ws::WebSocketSession> &ws_session,
 						const info::VHostAppName &vhost_app_name, const ov::String &host_name, const ov::String &stream_name,
-	                    const std::shared_ptr<RtcIceCandidate> &candidate,
-	                    const ov::String &username_fragment) override;
+						const std::shared_ptr<RtcIceCandidate> &candidate,
+						const ov::String &username_fragment) override;
 
 	bool OnStopCommand(const std::shared_ptr<http::svr::ws::WebSocketSession> &ws_session,
 					   const info::VHostAppName &vhost_app_name, const ov::String &host_name, const ov::String &stream_name,
-	                   const std::shared_ptr<const SessionDescription> &offer_sdp,
-	                   const std::shared_ptr<const SessionDescription> &peer_sdp) override;
-					   
+					   const std::shared_ptr<const SessionDescription> &offer_sdp,
+					   const std::shared_ptr<const SessionDescription> &peer_sdp) override;
+
 private:
 	enum class MessageCode : uint32_t
 	{
@@ -103,7 +104,7 @@ private:
 
 	std::shared_ptr<IcePort> _ice_port;
 	std::shared_ptr<RtcSignallingServer> _signalling_server;
-	ov::MessageThread<std::shared_ptr<ov::CommonMessage>>	_message_thread;
+	ov::MessageThread<std::shared_ptr<ov::CommonMessage>> _message_thread;
 
 	// for special purpose log - Deprecated
 	// ov::DelayQueue _timer;
