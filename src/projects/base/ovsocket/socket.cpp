4da852e7e0a191f4831f9460d31863d53e2ded2f
Dimiden <dimiden@airensoft.com>
2023-05-02 21:58:56 +0900

    Changed to set SRTO_IPV6ONLY flag for SRT

--- src/projects/base/ovsocket/socket.cpp
+++ src/projects/base/ovsocket/socket.cpp
@@ -196,19 +196,15 @@ namespace ov
 
 			SetState(SocketState::Created);
 
-			if (family == SocketFamily::Inet6)
+			if (type == SocketType::Srt)
+			{
+				SetSockOpt(SRTO_IPV6ONLY, 1);
+			}
+			else if (family == SocketFamily::Inet6)
 			{
 				// In some environments, listening to IPv6 will also listen to IPv4
 				// This setting lets this socket listen to IPv6 only
-
-				if (type == SocketType::Srt)
-				{
-					SetSockOpt(SRTO_IPV6ONLY, 1);
-				}
-				else
-				{
-					SetSockOpt(IPPROTO_IPV6, IPV6_V6ONLY, 1);
-				}
+				SetSockOpt(IPPROTO_IPV6, IPV6_V6ONLY, 1);
 			}
 
 			return true;
