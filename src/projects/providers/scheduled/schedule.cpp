49631c79a78728a6752affd08e1e7d5a4be7004a
getroot <getroot@airensoft.com>
2023-11-13 21:21:24 +0900

    Fix item duration to be at least 1000ms in schedule.cpp

--- src/projects/providers/scheduled/schedule.cpp
+++ src/projects/providers/scheduled/schedule.cpp
@@ -264,6 +264,12 @@ namespace pvd
 
 			item->duration_ms = duration_attribute.as_llong();
 
+			if (item->duration_ms <= 1000)
+			{
+				logtw("Item duration is too short, duration must be greater than 1000ms. url: %s, duration: %lld, it will be changed to 1000ms", item->url.CStr(), item->duration_ms);
+				item->duration_ms = 1000;
+			}
+
 			items.push_back(item);
 		}
 
