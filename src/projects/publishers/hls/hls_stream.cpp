00305327233d0c3d07a30d85d6e62202311ad8ed
getroot <getroot@airensoft.com>
2024-07-06 02:06:58 +0900

    Fixed a crash that occurred when the source track name in the mux file was set incorrectly

--- src/projects/publishers/hls/hls_stream.cpp
+++ src/projects/publishers/hls/hls_stream.cpp
@@ -172,7 +172,7 @@ bool HlsStream::CreateDefaultPlaylist()
 
 	if (first_video_track == nullptr && first_audio_track == nullptr)
 	{
-		logtw("HLS stream [%s/%s] could not be created because there is no supported codec.", GetApplication()->GetName().CStr(), GetName().CStr());
+		logtw("HLS stream [%s/%s] could not create default playlist.m3u8 because there is no supported codec.", GetApplication()->GetName().CStr(), GetName().CStr());
 		return false;
 	}
 
@@ -424,6 +424,15 @@ bool HlsStream::CreatePackagers()
 
 			auto video_variant_name = rendition->GetVideoVariantName();
 			auto audio_variant_name = rendition->GetAudioVariantName();
+
+			// Check if there is a track with the variant name
+			if ((video_variant_name.IsEmpty() == false && GetMediaTrackGroup(video_variant_name) == nullptr) || 
+				(audio_variant_name.IsEmpty() == false && GetMediaTrackGroup(audio_variant_name) == nullptr))
+			{
+				logtw("HLS Stream(%s/%s) - Invalid rendition %s. The variant name video(%s) audio(%s) is not found in the track list", GetApplication()->GetName().CStr(), GetName().CStr(), playlist->GetFileName().CStr(), video_variant_name.CStr(), audio_variant_name.CStr());
+				continue;
+			}
+
 			auto variant_name = GetVariantName(video_variant_name, audio_variant_name);
 
 			auto media_playlist = GetMediaPlaylist(variant_name);
