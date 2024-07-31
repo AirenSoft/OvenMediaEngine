df32bc5e1656f9dcc051aa30eae71bb89b347b07
Keukhan <soulk@airensoft.com>
2024-07-31 14:58:13 +0900

    Changed Jpeg pixel format to YUVJ420P

--- src/projects/transcoder/codec/encoder/encoder_jpeg.h
+++ src/projects/transcoder/codec/encoder/encoder_jpeg.h
@@ -25,7 +25,7 @@ public:
 
 	int GetSupportedFormat() const noexcept override
 	{
-		return AV_PIX_FMT_YUV420P;
+		return AV_PIX_FMT_YUVJ420P;
 	}
 
 	cmn::BitstreamFormat GetBitstreamFormat() const noexcept override
