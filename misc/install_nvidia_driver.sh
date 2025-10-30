fc09cb65986f56a423ce93dba5f165d387340531
getroot <getroot@airensoft.com>
2025-10-30 20:37:57 +0900

    Fixed command

--- misc/install_nvidia_driver.sh
+++ misc/install_nvidia_driver.sh
@@ -96,7 +96,7 @@ install_base_ubuntu()
             # Insall recommended version 470
             sudo apt-get install -y --no-install-recommends nvidia-driver-470
 			# Install CUDA Toolkit 11.8
-			wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+			curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 			dpkg -i cuda-keyring_1.1-1_all.deb
 			apt-get update
 			apt-get -y install cuda-toolkit-11-8
