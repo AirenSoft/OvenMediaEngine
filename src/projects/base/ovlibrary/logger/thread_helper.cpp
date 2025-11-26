824f412c6b425e4606404c4e935fb06d6f6003f4
Hyunjun Jang <dimiden@airensoft.com>
2025-11-26 22:22:34 +0900

    Fixed timing issue when exiting OME

--- src/projects/base/ovlibrary/logger/thread_helper.cpp
+++ src/projects/base/ovlibrary/logger/thread_helper.cpp
@@ -14,22 +14,76 @@
 
 namespace ov::logger
 {
+	namespace internal
+	{
+		class Helper
+		{
+		public:
+			void AppendThreadInfo(size_t thread_id, pthread_t pthread_id)
+			{
+				std::lock_guard lock(_thread_id_map_mutex);
+				(*_thread_id_map)[thread_id] = pthread_id;
+			}
+
+			void RemoveThreadInfo(size_t thread_id)
+			{
+				std::lock_guard lock(_thread_id_map_mutex);
+
+#if DEBUG
+				if (_thread_id_map->find(thread_id) == _thread_id_map->end())
+				{
+					OV_ASSERT(false, "Thread id not found in the map.");
+				}
+#endif	// DEBUG
+
+				_thread_id_map->erase(thread_id);
+			}
+
+			pthread_t GetPthreadId(size_t thread_id)
+			{
+				std::shared_lock lock(_thread_id_map_mutex);
+
+				auto it = _thread_id_map->find(thread_id);
+
+				if (it != _thread_id_map->end())
+				{
+					return it->second;
+				}
+
+#if DEBUG
+				OV_ASSERT(false, "Thread id not found in the map.");
+#endif	// DEBUG
+
+				return 0;
+			}
+
+		private:
+			std::shared_mutex _thread_id_map_mutex;
+			// key: `thread_id` - This is obtained by calling `thread_id()` of `spdlog`.
+			// value: `pthread_id`
+			std::shared_ptr<std::unordered_map<size_t, pthread_t>> _thread_id_map = std::make_shared<std::unordered_map<size_t, pthread_t>>();
+		};
+
+		static std::shared_ptr<Helper> &GetHelper()
+		{
+			static auto instance = std::make_shared<Helper>();
+			return instance;
+		}
+	}  // namespace internal
+
 	ThreadHelper::ThreadHelper()
 	{
-		std::lock_guard lock(_thread_id_map_mutex);
+		_helper = internal::GetHelper();
+		_helper->AppendThreadInfo(spdlog::details::os::thread_id(), ::pthread_self());
 
-#ifdef DEBUG
+#if DEBUG
 		_thread_id	= spdlog::details::os::thread_id();
 		_pthread_id = ::pthread_self();
 #endif	// DEBUG
-
-		_thread_id_map[spdlog::details::os::thread_id()] = ::pthread_self();
 	}
 
 	ThreadHelper::~ThreadHelper()
 	{
-		std::lock_guard lock(_thread_id_map_mutex);
-
 #if DEBUG
 		auto thread_id	= spdlog::details::os::thread_id();
 		auto pthread_id = ::pthread_self();
@@ -43,34 +97,20 @@ namespace ov::logger
 		{
 			OV_ASSERT(false, "pthread_id mismatch in destructor. Expected: %lu, Actual: %lu", static_cast<unsigned long>(_pthread_id), static_cast<unsigned long>(pthread_id));
 		}
-
-		if (_thread_id_map.find(thread_id) == _thread_id_map.end())
-		{
-			OV_ASSERT(false, "Thread id not found in the map.");
-		}
 #endif	// DEBUG
 
-		_thread_id_map.erase(spdlog::details::os::thread_id());
+		_helper->RemoveThreadInfo(spdlog::details::os::thread_id());
 	}
 
 	pthread_t ThreadHelper::GetPthreadId(size_t thread_id)
 	{
-		std::shared_lock lock(_thread_id_map_mutex);
+		auto helper = internal::GetHelper();
 
-		auto it = _thread_id_map.find(thread_id);
-
-		if (it != _thread_id_map.end())
+		if (helper != nullptr)
 		{
-			return it->second;
+			return helper->GetPthreadId(thread_id);
 		}
-
-#if DEBUG
-		OV_ASSERT(false, "Thread id not found in the map.");
-#endif	// DEBUG
 
 		return 0;
 	}
-
-	std::unordered_map<size_t, pthread_t> ThreadHelper::_thread_id_map;
-	std::shared_mutex ThreadHelper::_thread_id_map_mutex;
 }  // namespace ov::logger
--- src/projects/base/ovlibrary/logger/thread_helper.h
+++ src/projects/base/ovlibrary/logger/thread_helper.h
@@ -16,11 +16,18 @@ namespace ov::logger
 {
 	constexpr size_t MAX_THREAD_NAME_LENGTH = 16;
 
+	namespace internal
+	{
+		class Helper;
+	}
+
 	// This class maps spdlog's thread id to pthread id.
 	// Usage: When a new thread is created, just declare this variable at the entry of the thread proc.
 	// Upon declaration, the constructor adds the mapping, and later the destructor removes it by RAII.
 	class ThreadHelper
 	{
+		friend class Helper;
+		
 	public:
 		ThreadHelper();
 		~ThreadHelper();
@@ -28,14 +35,11 @@ namespace ov::logger
 		static pthread_t GetPthreadId(size_t thread_id);
 
 	private:
-		// key: `thread_id` - This is obtained by calling `thread_id()` of `spdlog`.
-		// value: `pthread_id`
-		static std::unordered_map<size_t, pthread_t> _thread_id_map;
-		static std::shared_mutex _thread_id_map_mutex;
-
 #ifdef DEBUG
 		size_t _thread_id;
 		pthread_t _pthread_id;
 #endif	// DEBUG
+
+		std::shared_ptr<internal::Helper> _helper;
 	};
 }  // namespace ov::logger
