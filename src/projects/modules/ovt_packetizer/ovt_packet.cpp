3eeb3b62e210d86ae4cf90a593582ac9c8b13762
getroot <getroot@airensoft.com>
2025-12-17 14:16:27 +0900

    Fixed a potential crash when malicious packets were sent to OVT pub

--- src/projects/modules/ovt_packetizer/ovt_packet.cpp
+++ src/projects/modules/ovt_packetizer/ovt_packet.cpp
@@ -1,20 +1,21 @@
-#include <base/ovlibrary/byte_io.h>
 #include "ovt_packet.h"
+
+#include <base/ovlibrary/byte_io.h>
 
 OvtPacket::OvtPacket()
 {
-	_version = OVT_VERSION;
-	_marker = 0;
-	_payload_type = 0;
-	_timestamp = 0;
+	_version		 = OVT_VERSION;
+	_marker			 = 0;
+	_payload_type	 = 0;
+	_timestamp		 = 0;
 	_sequence_number = 0;
-	_session_id = 0;
-	_payload_length = 0;
+	_session_id		 = 0;
+	_payload_length	 = 0;
 
-	_data = std::make_shared<ov::Data>();
+	_data			 = std::make_shared<ov::Data>();
 	_data->Reserve(OVT_DEFAULT_MAX_PACKET_SIZE);
 	_data->SetLength(OVT_FIXED_HEADER_SIZE);
-	_buffer = _data->GetWritableDataAs<uint8_t>();
+	_buffer	   = _data->GetWritableDataAs<uint8_t>();
 
 	_buffer[0] = OVT_VERSION << 6;
 }
@@ -26,16 +27,16 @@ OvtPacket::OvtPacket(const ov::Data &data)
 
 OvtPacket::OvtPacket(OvtPacket &src)
 {
-	_version = OVT_VERSION;
-	_marker = src._marker;
-	_payload_type = src._payload_type;
-	_timestamp = src._timestamp;
-	_sequence_number = src._sequence_number;
-	_session_id = src._session_id;
-	_payload_length = src._payload_length;
+	_version			 = OVT_VERSION;
+	_marker				 = src._marker;
+	_payload_type		 = src._payload_type;
+	_timestamp			 = src._timestamp;
+	_sequence_number	 = src._sequence_number;
+	_session_id			 = src._session_id;
+	_payload_length		 = src._payload_length;
 	_is_packet_available = src._is_packet_available;
-	
-	_data = src._data->Clone();
+
+	_data				 = src._data->Clone();
 	_data->SetLength(src._data->GetLength());
 	_buffer = _data->GetWritableDataAs<uint8_t>();
 }
@@ -46,7 +47,7 @@ OvtPacket::~OvtPacket()
 
 bool OvtPacket::LoadHeader(const ov::Data &data)
 {
-	if(data.GetLength() < OVT_FIXED_HEADER_SIZE)
+	if (data.GetLength() < OVT_FIXED_HEADER_SIZE)
 	{
 		_is_packet_available = false;
 		return false;
@@ -55,7 +56,7 @@ bool OvtPacket::LoadHeader(const ov::Data &data)
 	auto buffer = data.GetDataAs<uint8_t>();
 
 	uint8_t version = (buffer[0] & 0xC0) >> 6;
-	if(version != OVT_VERSION)
+	if (version != OVT_VERSION)
 	{
 		loge("ERROR", "Version : %d, %d", buffer[0], version);
 		_is_packet_available = false;
@@ -77,9 +78,17 @@ bool OvtPacket::LoadHeader(const ov::Data &data)
 	SetSequenceNumber(ByteReader<uint16_t>::ReadBigEndian(&buffer[2]));
 	SetTimestamp(ByteReader<uint64_t>::ReadBigEndian(&buffer[4]));
 	SetSessionId(ByteReader<uint32_t>::ReadBigEndian(&buffer[12]));
-	SetPayloadLength(ByteReader<uint16_t>::ReadBigEndian(&buffer[16]));
 
-	if(PayloadLength() == 0)
+	uint16_t payload_length = ByteReader<uint16_t>::ReadBigEndian(&buffer[16]);
+	if (payload_length > OVT_DEFAULT_MAX_PACKET_SIZE - OVT_FIXED_HEADER_SIZE)
+	{
+		loge("ERROR", "Invalid payload length : %u", payload_length);
+		_is_packet_available = false;
+		return false;
+	}
+	SetPayloadLength(payload_length);
+
+	if (PayloadLength() == 0)
 	{
 		_is_packet_available = true;
 	}
@@ -88,19 +97,18 @@ bool OvtPacket::LoadHeader(const ov::Data &data)
 		// this packet is unavailable until setiing payload
 		_is_packet_available = false;
 	}
-	
 
 	return true;
 }
 
 bool OvtPacket::Load(const ov::Data &data)
 {
-	if(!LoadHeader(data))
+	if (!LoadHeader(data))
 	{
 		return false;
 	}
 
-	if(data.GetLength() < static_cast<size_t>(OVT_FIXED_HEADER_SIZE + _payload_length))
+	if (data.GetLength() < static_cast<size_t>(OVT_FIXED_HEADER_SIZE + _payload_length))
 	{
 		// Invalid data
 		_is_packet_available = false;
@@ -115,7 +123,7 @@ bool OvtPacket::Load(const ov::Data &data)
 	return true;
 }
 
-bool OvtPacket::IsHeaderAvailable() const 
+bool OvtPacket::IsHeaderAvailable() const
 {
 	return _payload_type > 0;
 }
@@ -165,17 +173,17 @@ uint16_t OvtPacket::PayloadLength() const
 	return _payload_length;
 }
 
-const uint8_t* OvtPacket::Payload() const
+const uint8_t *OvtPacket::Payload() const
 {
 	return &_buffer[OVT_FIXED_HEADER_SIZE];
 }
 
-const uint8_t* OvtPacket::GetBuffer() const
+const uint8_t *OvtPacket::GetBuffer() const
 {
 	return &_buffer[0];
 }
 
-const std::shared_ptr<ov::Data>& OvtPacket::GetData() const
+const std::shared_ptr<ov::Data> &OvtPacket::GetData() const
 {
 	return _data;
 }
@@ -189,7 +197,7 @@ void OvtPacket::SetMarker(bool marker_bit)
 {
 	_marker = marker_bit;
 
-	if(_marker)
+	if (_marker)
 	{
 		_buffer[0] = _buffer[0] | 0x20;
 	}
@@ -202,7 +210,7 @@ void OvtPacket::SetMarker(bool marker_bit)
 void OvtPacket::SetPayloadType(uint8_t payload_type)
 {
 	_payload_type = payload_type;
-	_buffer[1] = payload_type;
+	_buffer[1]	  = payload_type;
 }
 
 void OvtPacket::SetSequenceNumber(uint16_t sequence_number)
@@ -214,7 +222,7 @@ void OvtPacket::SetSequenceNumber(uint16_t sequence_number)
 void OvtPacket::SetTimestampNow()
 {
 	using namespace std::chrono;
-	uint64_t 	timestamp = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+	uint64_t timestamp = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 	SetTimestamp(timestamp);
 }
 
@@ -236,14 +244,15 @@ void OvtPacket::SetPayloadLength(size_t payload_length)
 	ByteWriter<uint16_t>::WriteBigEndian(&_buffer[16], _payload_length);
 
 	_data->SetLength(OVT_FIXED_HEADER_SIZE + payload_length);
+	_buffer = _data->GetWritableDataAs<uint8_t>();
 }
 
 bool OvtPacket::SetPayload(const uint8_t *payload, size_t payload_length)
 {
-	if(OVT_FIXED_HEADER_SIZE + payload_length > _data->GetCapacity())
+	if (OVT_FIXED_HEADER_SIZE + payload_length > _data->GetCapacity())
 	{
 		OV_ASSERT(false, "Data capacity must be greater than %ld (packet : %ld)",
-				_data->GetCapacity(), OVT_FIXED_HEADER_SIZE + payload_length);
+				  _data->GetCapacity(), OVT_FIXED_HEADER_SIZE + payload_length);
 		return false;
 	}
 
